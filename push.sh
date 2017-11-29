e324256fd6101c85d700549ce4b7d3565b51c592
helongquan <791447771@qq.com>
2017-11-29 18:08:11 +0800

    对必读文件进行了一个修改

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git pull
 echo "Start to pull from remote...\n"
 git add .
 echo "add the change file to local reposit"
-git commit -m "update"
+git commit -m "$1"
 echo "commit the change to version reposit"
 git push origin master
 echo "finished !"
